b920b37d787e4cb4f4ffe7a91656633201122117
Benjamin Ramauge <ramauge.benjamin@gmail.com>
2020-11-13 20:30:53 +0100

    First commit

---
+++ commands/poll.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const EmojiRegex = require('emoji-regex/RGI_Emoji.js');
+
+module.exports = {
+    name: 'poll',
+    description: 'Simple way to create poll with emoji',
+    async execute(message, args){
+        const emotRegex = EmojiRegex();
+        const emojiProp = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+        
+        //Récupération des éléments entre quotes
+        let options = message.content.match(/".*?"/g);
+        options = options.map(x => x.replace(/"/g, '')).filter(x => x != '');
+        
+        //Récupération de la question
+        const question = `📊 ${options.shift()}`;
+        
+        var emojis = [];
+
+        let response = options.forEach((element, index) => {
+            let match = emotRegex.exec(element)
+
+            if(match != null){
+                emojis.push(match[0]);
+            }else{
+                let emoji = emojiProp.shift();
+                emojis.push(emoji);
+                options[index] = `${element} ${emoji}`;
+            }
+
+        }, options);
+
+        let response = options.join('\n');
+
+        //Create embed message
+        const embedMessage = new Discord.MessageEmbed()
+            .setColor('#8e24aa')
+            .setAuthor('Sondage')
+            .setTitle(question)
+            .setDescription(response)
+            .setTimestamp()
+            .setFooter(`De ${message.author.username}`);        
+
+        //Envoi message message
+        let sendedMessage = await message.channel.send(embedMessage);
+
+        //Affectation des réactions
+        emojis.forEach(emote => {
+            sendedMessage.react(emote)
+            .catch((error) => console.error(error + " Emoji not found"));
+        });
+    }
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const Discord = require("discord.js");
+const { prefix, token} = require("./config.json");
+
+//Permet d'intéragir avec le client discord
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on("message", async (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    //const commandBody = message.content.slice(prefix.length);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if(!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Une erreur est survenue lors de l\'exécution de la commande');
+    }
+});
+
+//Permet au bot de se connecter avec son token
+client.login(token);
